6a8f827dac01e63e67e750783b5f01260c221df2
Takahiro Kawashima <takah.kawashima@gmail.com>
2022-09-20 20:35:25 +0900

    fix bugs

--- lrdpp_estimation.jl
+++ lrdpp_estimation.jl
@@ -18,7 +18,7 @@ function update_V(Vₜ, samples, U_samples, M, ρₜ = 1.0)
     Lₜ = Vₜ * Vₜ'
     term1 = -mean([U_samples[m]' * inv(Lₜ[samples[m], samples[m]]) * U_samples[m] for m in 1:M])
     term2 = inv(Lₜ + I)
-    return ρₜ * (term1 + term2 + ρₜ * I) \ Vₜ
+    return ((term1 + term2 + ρₜ * I) / ρₜ) \ Vₜ
 end
 
 function logdet_divergence(X, Y)
@@ -38,35 +38,43 @@ L = rand(Wishart(N, diagm(1:N) / N)) / N
 dpp = DeterminantalPointProcess(L)
 samples = rand(dpp, M)
 
-Linit = rand(Wishart(10N, diagm(10 * ones(N)))) / 10N
-#Linit = rand(Wishart(10N, diagm(5 * ones(N)))) / 10N
+K = min(maximum(length.(samples)), N)
+
+#Linit = rand(Wishart(10N, diagm(10 * ones(N)))) / 10N
+Linit = rand(Wishart(10N, diagm(5 * ones(N)))) / 10N
+#Linit = rand(Wishart(10N, diagm(ones(N)))) / 10N
 eig_init = eigen(Linit)
 Vinit = eig_init.vectors[:, end - K + 1:end] * Diagonal(sqrt.(eig_init.values[end - K + 1:end]))
 
 U_samples = [I(N)[sample, :] for sample in samples]
 
 
-Vest = Vinit
+ρs = [5.0, 2.0, 1.0]
 n_iter = 100
-logliks = zeros(n_iter)
-logliks[1] = compute_loglik(Vest, samples)
-lddiv_numer = zeros(n_iter - 1)
-lddiv_denom = zeros(n_iter - 1)
-lddiv_numer_prox = zeros(n_iter - 1)
+logliks = zeros(n_iter, length(ρs))
+logliks[1, :] .= compute_loglik(Vinit, samples)
+lddiv_numer = zeros(n_iter - 1, length(ρs))
+lddiv_denom = zeros(n_iter - 1, length(ρs))
+lddiv_numer_prox = zeros(n_iter - 1, length(ρs))
 sval_traj = zeros(K, n_iter - 1)
 
-ρ = 1.0
-@showprogress for i in 2:n_iter
-    try
-        Vnext = update_V(Vest, samples, U_samples, M, ρ)
-        logliks[i] = compute_loglik(Vnext, samples)
-        lddiv_numer[i - 1] = mean([logdet_divergence((Vnext * Vnext')[sample, sample], (Vest * Vest')[sample, sample]) for sample in samples])
-        lddiv_denom[i - 1] = logdet_divergence(Vnext * Vnext' + I, Vest * Vest' + I)
-        lddiv_numer_prox[i - 1] = lddiv_numer[i - 1] - ρ * norm(Vnext - Vest, 2)
-        sval_traj[:, i - 1] = svdvals(Vnext)
-        Vest = Vnext
-    catch
-        break
+Vest = Vinit
+for (j, ρ) in enumerate(ρs)
+    Vest = Vinit
+    @showprogress for i in 2:n_iter
+        try
+            Vnext = update_V(Vest, samples, U_samples, M, ρ)
+            logliks[i, j] = compute_loglik(Vnext, samples)
+            lddiv_numer[i - 1, j] = mean([logdet_divergence((Vnext * Vnext')[sample, sample], (Vest * Vest')[sample, sample]) for sample in samples])
+            lddiv_denom[i - 1, j] = logdet_divergence(Vnext * Vnext' + I, Vest * Vest' + I)
+            lddiv_numer_prox[i - 1, j] = lddiv_numer[i - 1, j] - ρ * norm(Vnext - Vest, 2)
+            if (ρ == 1.0)
+                sval_traj[:, i - 1] = svdvals(Vnext)
+            end
+            Vest = Vnext
+        catch
+            break
+        end
     end
 end
 
@@ -75,15 +83,17 @@ loglik_truth = sum([logpdf(DeterminantalPointProcess(L), sample) for sample in s
 cmin = minimum(hcat(L, Vinit * Vinit', Vest * Vest'))
 cmax = maximum(hcat(L, Vinit * Vinit', Vest * Vest'))
 
-p1 = plot(logliks, ylabel = "log-likelihood", xlabel = "iter.", legend = :none, dpi = 200)
-hline!(p1, [loglik_truth])
+p1 = plot(logliks, ylabel = "log-likelihood", xlabel = "iter.", legend = :bottomright, dpi = 200,
+          label = ["rho = 5" "rho = 2" "rho = 1"], margin = 5Plots.mm, lw = 2)
+hline!(p1, [loglik_truth], label = "true param.", lw = 2)
 p2 = heatmap(L, clims = (cmin, cmax), title = "truth", dpi = 200)
 p3 = heatmap(Vinit * Vinit', clims = (cmin, cmax), title = "init.", dpi = 200)
-p4 = heatmap(Vest * Vest', clims = (cmin, cmax), title = "est.", dpi = 200)
+p4 = heatmap(Vest * Vest', clims = (cmin, cmax), title = "est. (rho = 1)", dpi = 200)
 p = plot(p1, p2, p3, p4, size = (1200, 800))
-savefig(p, "result.png")
+savefig(p, "result.pdf")
 
-p1 = plot(hcat(lddiv_numer, lddiv_denom, lddiv_numer_prox), label = ["numer" "denom" "numer+prox"], title = "LogDet Div.")
-p2 = plot(sval_traj', legend = :none, title = "trajectory of svdvals")
-plot(p1, p2)
-
+plots_lddiv = [plot(hcat(lddiv_numer[:, j], lddiv_denom[:, j], lddiv_numer_prox[:, j]),
+                    label = ["numer" "denom" "numer+prox"], title = "LogDet Div. (rho = $ρ)",
+                    dpi = 200, legend = :bottomright)
+               for (j, ρ) in enumerate(ρs)]
+savefig.(plots_lddiv, ["result_rho$(ρ).pdf" for ρ in ρs])
