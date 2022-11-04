63706115bfd470fc6b61a596e57ea8fdeb01d344
Takahiro <takah.kawashima@gmail.com>
2022-11-04 17:35:24 +0900

    implement a struct for storing learning results

--- lrdpp_estimation.jl
+++ lrdpp_estimation.jl
@@ -20,6 +20,21 @@ mutable struct LFDPP
 end
 
 
+struct LFDPPResult
+    samples :: Vector{Vector{Int64}}
+    lfdpp :: LFDPP
+    loglik :: Float64
+    lfdpp_trace :: Vector{LFDPP}
+    loglik_trace :: Vector{Float64}
+    n_iter :: Int64
+
+    function LFDPPResult(lfdpp_trace, samples)
+        loglik_trace = map(lfdpp -> compute_loglik(lfdpp, samples), lfdpp_trace)
+        new(samples, lfdpp_trace[end], loglik_trace[end], lfdpp_trace, loglik_trace, length(lfdpp_trace))
+    end
+end
+
+
 function compute_loglik(lfdpp :: LFDPP, samples)
     logZ = logdet(lfdpp.V' * lfdpp.V + I)
     return sum([logdet(lfdpp.V[sample, :] * lfdpp.V[sample, :]') for sample in samples]) - length(samples) * logZ
@@ -55,17 +70,15 @@ function mle(lfdpp :: LFDPP, samples; n_iter = 100, ρ = 1.0, show_progress = tr
     # MLE for a low-rank factorized DPP by the proposed method
     prog = Progress(n_iter - 1, enabled = show_progress)
 
-    logliks = zeros(n_iter)
-    logliks[1] = compute_loglik(lfdpp, samples)
+    lfdpp_trace = Vector{LFDPP}(undef, n_iter)
+    lfdpp_trace[1] = lfdpp
 
     for i in 2:n_iter
-        lfdpp_next = LFDPP(update_V(lfdpp.V, samples, ρ))
-        logliks[i] = compute_loglik(lfdpp_next, samples)
-        lfdpp = lfdpp_next
+        lfdpp_trace[i] = LFDPP(update_V(lfdpp_trace[i - 1].V, samples, ρ))
 
         next!(prog)
     end
-    return lfdpp, logliks
+    return LFDPPResult(lfdpp_trace, samples)
 end
 
 function mle_grad(lfdpp :: LFDPP, samples; n_iter = 100, η = 1.0, show_progress = true)
@@ -114,20 +127,21 @@ eig_init = eigen(Linit)
 Vinit = eig_init.vectors[:, end - K + 1:end] * Diagonal(sqrt.(eig_init.values[end - K + 1:end]))
 
 
-lfdpp1, logp1 = mle(LFDPP(Vinit), samples, ρ = 1.0)
-lfdpp2, logp2 = mle(LFDPP(Vinit), samples, ρ = 2.0)
-lfdpp5, logp5 = mle(LFDPP(Vinit), samples, ρ = 5.0)
+lfdpp_res1 = mle(LFDPP(Vinit), samples, ρ = 1.0)
+lfdpp_res2 = mle(LFDPP(Vinit), samples, ρ = 2.0)
+lfdpp_res5 = mle(LFDPP(Vinit), samples, ρ = 5.0)
 
 loglik_truth = sum([logpdf(DeterminantalPointProcess(L), sample) for sample in samples])
-cmin = minimum(hcat(L, Vinit * Vinit', lfdpp1.V * lfdpp1.V', lfdpp2.V * lfdpp2.V', lfdpp5.V * lfdpp5.V'))
-cmax = maximum(hcat(L, Vinit * Vinit', lfdpp1.V * lfdpp1.V', lfdpp2.V * lfdpp2.V', lfdpp5.V * lfdpp5.V'))
+cmin = minimum(hcat(L, Vinit * Vinit', lfdpp_res1.lfdpp.V * lfdpp_res1.lfdpp.V', lfdpp_res2.lfdpp.V * lfdpp_res2.lfdpp.V', lfdpp_res5.lfdpp.V * lfdpp_res5.lfdpp.V'))
+cmax = maximum(hcat(L, Vinit * Vinit', lfdpp_res1.lfdpp.V * lfdpp_res1.lfdpp.V', lfdpp_res2.lfdpp.V * lfdpp_res2.lfdpp.V', lfdpp_res5.lfdpp.V * lfdpp_res5.lfdpp.V'))
 
-p1 = plot([logp1, logp2, logp5], ylabel = "log-likelihood", xlabel = "iter.", legend = :bottomright, dpi = 200,
+p1 = plot([lfdpp_res1.loglik_trace, lfdpp_res2.loglik_trace, lfdpp_res5.loglik_trace],
+          ylabel = "log-likelihood", xlabel = "iter.", legend = :bottomright, dpi = 200,
           label = ["rho = 1" "rho = 2" "rho = 5"], margin = 5Plots.mm, lw = 2)
 hline!(p1, [loglik_truth], label = "true param.", lw = 2)
 p2 = heatmap(L, clims = (cmin, cmax), title = "truth", dpi = 200)
 p3 = heatmap(Vinit * Vinit', clims = (cmin, cmax), title = "init.", dpi = 200)
-p4 = heatmap(lfdpp1.V * lfdpp1.V', clims = (cmin, cmax), title = "est. (rho = 1)", dpi = 200)
+p4 = heatmap(lfdpp_res1.lfdpp.V * lfdpp_res1.lfdpp.V', clims = (cmin, cmax), title = "est. (rho = 1)", dpi = 200)
 p = plot(p1, p2, p3, p4, size = (1200, 800))
 savefig(p, "result.pdf")
 
