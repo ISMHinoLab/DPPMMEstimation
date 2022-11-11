087854e72a942c26628dd59680bc1e9ee0a6e9de
wasyro <takah.kawashima@gmail.com>
2022-11-11 19:44:54 +0900

    adjust the experiments for IBIS2022

--- lrdpp_estimation.jl
+++ lrdpp_estimation.jl
@@ -26,23 +26,28 @@ struct LFDPPResult
     loglik :: Float64
     lfdpp_trace :: Vector{LFDPP}
     loglik_trace :: Vector{Float64}
+    cputime_trace :: Vector{Float64}
     n_iter :: Int64
 
     function LFDPPResult(lfdpp_trace, samples)
         loglik_trace = map(lfdpp -> compute_loglik(lfdpp, samples), lfdpp_trace)
-        new(samples, lfdpp_trace[end], loglik_trace[end], lfdpp_trace, loglik_trace, length(lfdpp_trace))
+        new(samples, lfdpp_trace[end], loglik_trace[end], lfdpp_trace, loglik_trace, zeros(), length(lfdpp_trace))
+    end
+
+    function LFDPPResult(lfdpp_trace, samples, cputime_trace)
+        loglik_trace = map(lfdpp -> compute_loglik(lfdpp, samples), lfdpp_trace)
+        new(samples, lfdpp_trace[end], loglik_trace[end], lfdpp_trace, loglik_trace, cputime_trace, length(lfdpp_trace))
     end
 end
 
 
 function compute_loglik(lfdpp :: LFDPP, samples)
     logZ = logdet(lfdpp.V' * lfdpp.V + I)
-    return sum([logdet(lfdpp.V[sample, :] * lfdpp.V[sample, :]') for sample in samples]) - length(samples) * logZ
-    #try
-    #    return sum([logdet(lfdpp.V[sample, :] * lfdpp.V[sample, :]') for sample in samples]) - length(samples) * logZ
-    #catch
-    #    return -Inf
-    #end
+    try
+        return sum([logdet(lfdpp.V[sample, :] * lfdpp.V[sample, :]') for sample in samples]) - length(samples) * logZ
+    catch
+        return -Inf
+    end
 end
 
 function update_V(V, samples, ρ = 1.0)
@@ -72,32 +77,35 @@ function mle(lfdpp :: LFDPP, samples; n_iter = 100, ρ = 1.0, show_progress = tr
 
     lfdpp_trace = Vector{LFDPP}(undef, n_iter)
     lfdpp_trace[1] = lfdpp
+    cputime_trace = zeros(n_iter)
 
     for i in 2:n_iter
-        lfdpp_trace[i] = LFDPP(update_V(lfdpp_trace[i - 1].V, samples, ρ))
-
+        cputime_trace[i] = @elapsed begin
+            lfdpp_trace[i] = LFDPP(update_V(lfdpp_trace[i - 1].V, samples, ρ))
+        end
         next!(prog)
     end
-    return LFDPPResult(lfdpp_trace, samples)
+    return LFDPPResult(lfdpp_trace, samples, cumsum(cputime_trace))
 end
 
 function mle_grad(lfdpp :: LFDPP, samples; n_iter = 100, η = 1.0, show_progress = true)
     # MLE for a low-rank factorized DPP by gradient ascent
     prog = Progress(n_iter - 1, enabled = show_progress)
 
-    logliks = zeros(n_iter)
-    logliks[1] = compute_loglik(lfdpp, samples)
+    lfdpp_trace = Vector{LFDPP}(undef, n_iter)
+    lfdpp_trace[1] = lfdpp
+    cputime_trace = zeros(n_iter)
 
     for i in 2:n_iter
-        gradV = grad_V(lfdpp.V, samples)
-        Vnext = lfdpp.V + η * gradV
-        lfdpp_next = LFDPP(Vnext)
-        logliks[i] = compute_loglik(lfdpp_next, samples)
-        lfdpp = lfdpp_next
+        cputime_trace[i] = @elapsed begin
+            gradV = grad_V(lfdpp_trace[i - 1].V, samples)
+            Vnext = lfdpp_trace[i - 1].V + η * gradV
+            lfdpp_trace[i] = LFDPP(Vnext)
+        end
 
         next!(prog)
     end
-    return lfdpp, logliks
+    return LFDPPResult(lfdpp_trace, samples, cumsum(cputime_trace))
 end
 
 
@@ -113,16 +121,20 @@ M = 1000
 
 #L = rand(Wishart(10N, diagm(1:N) / 10)) / 10N
 #L = rand(Wishart(N, diagm(10.0 .^ range(-3, 1, length = N)))) / N
-L = rand(Wishart(N, diagm(1:N) / N)) / N
-#V = rand(Uniform(0, √2), (N, round(Int, N/2))) / N; L = V * V'
+#L = rand(Wishart(N, diagm(1:N) / N)) / N
+V = rand(Uniform(0, 10), (N, round(Int, N/2))) / N; L = V * V'
 
 dpp = DeterminantalPointProcess(L)
 samples = rand(dpp, M)
 K = min(maximum(length.(samples)) + 1, N)
 
-#Linit = rand(Wishart(10N, diagm(10 * ones(N)))) / 10N
-Linit = rand(Wishart(10N, diagm(5 * ones(N)))) / 10N
+emp_probs = [mean(in.(n, samples)) for n in 1:N]
+init_diag = emp_probs ./ (1 .- emp_probs)
+#Linit = rand(Wishart(10N, diagm(10 * init_diag))) / 10N
+Linit = rand(Wishart(10N, diagm(10 * ones(N)))) / 10N
+#Linit = rand(Wishart(10N, diagm(5 * ones(N)))) / 10N
 #Linit = rand(Wishart(10N, diagm(ones(N)))) / 10N
+#Vinit = rand(Uniform(0, √2), (N, round(Int, N/2))) / N; Linit = Vinit * Vinit' * 1e2
 eig_init = eigen(Linit)
 Vinit = eig_init.vectors[:, end - K + 1:end] * Diagonal(sqrt.(eig_init.values[end - K + 1:end]))
 
@@ -147,8 +159,30 @@ savefig(p, "result.pdf")
 
 
 # 勾配法との比較
-lfdpp_grad1, logp_grad1 = mle_grad(LFDPP(Vinit), samples, η = 0.5)
-lfdpp_grad5, logp_grad5 = mle_grad(LFDPP(Vinit), samples, η = 0.1)
+lfdpp_grad1 = mle_grad(LFDPP(Vinit), samples, η = 0.5)
+lfdpp_grad2 = mle_grad(LFDPP(Vinit), samples, η = 0.25)
+lfdpp_grad5 = mle_grad(LFDPP(Vinit), samples, η = 0.1)
+loglik_min = minimum(hcat(lfdpp_res1.loglik_trace, lfdpp_grad1.loglik_trace,
+                          lfdpp_res5.loglik_trace, lfdpp_grad5.loglik_trace))
+loglik_max = maximum(hcat(lfdpp_res1.loglik_trace, lfdpp_grad1.loglik_trace,
+                          lfdpp_res5.loglik_trace, lfdpp_grad5.loglik_trace))
+p = plot([lfdpp_res1.loglik_trace, lfdpp_grad1.loglik_trace],
+         ylabel = "log-likelihood", xlabel = "#iter.", legend = :bottomright, dpi = 200,
+         ylims = (loglik_min, loglik_max),
+         label = ["proposed" "gradient ascent"], margin = 5Plots.mm, lw = 2)
+hline!(p, [loglik_truth], label = "true param.", lw = 2)
+savefig(p, "result_rho1.pdf")
+p = plot([lfdpp_res5.loglik_trace, lfdpp_grad5.loglik_trace],
+         ylabel = "log-likelihood", xlabel = "#iter.", legend = :bottomright, dpi = 200,
+         ylims = (loglik_min, loglik_max),
+         label = ["proposed" "gradient ascent"], margin = 5Plots.mm, lw = 2)
+hline!(p, [loglik_truth], label = "true param.", lw = 2)
+savefig(p, "result_rho5.pdf")
 
-plot([logp1, logp_grad1])
-plot([logp5, logp_grad5])
+plot(lfdpp_res1.cputime_trace, lfdpp_res1.loglik_trace)
+plot!(lfdpp_grad1.cputime_trace, lfdpp_grad1.loglik_trace)
+plot(lfdpp_res2.cputime_trace, lfdpp_res2.loglik_trace)
+plot!(lfdpp_grad2.cputime_trace, lfdpp_grad2.loglik_trace)
+plot(lfdpp_res5.cputime_trace, lfdpp_res5.loglik_trace)
+plot!(lfdpp_grad5.cputime_trace, lfdpp_grad5.loglik_trace)
+
